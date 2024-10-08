43a9faef19cac37fa243f94f01bd8965960a01ab
tbraunzech <tobias.braun@wf-ib.de>
2024-10-08 11:59:13 +0200

    2nd cycle

--- resources.tf
+++ resources.tf
@@ -265,7 +265,7 @@ resource "azurerm_automation_schedule" "sftp_enable_daily_second" {
   frequency               = var.sftp_enable_frequency
   interval                = var.interval
   timezone                = var.region_timezone_map[var.region]
-  start_time              = var.second_schedule_start == null ? "${local.update_date}T${local.update_time_start_new}:00+02:00" : null
+  start_time              = var.second_schedule_start != null ? "${local.update_date}T${local.update_time_start_new}:00+02:00" : null
   expiry_time             = var.expiry_time != null ? var.expiry_time : null
   description             = "Start of 2nd daily SFTP Cycle"
   week_days               = var.week_days != null ? var.week_days : null
@@ -293,7 +293,7 @@ resource "azurerm_automation_schedule" "sftp_disable_daily_second" {
   frequency               = var.sftp_enable_frequency
   interval                = var.interval
   timezone                = var.region_timezone_map[var.region]
-  start_time              = var.second_schedule_start == null ? "${local.update_date}T${local.update_time_stop_new}:00+02:00" : null
+  start_time              = var.second_schedule_start != null ? "${local.update_date}T${local.update_time_stop_new}:00+02:00" : null
   expiry_time             = var.expiry_time != null ? var.expiry_time : null
   description             = "End of daily SFTP Cycle"
   week_days               = var.week_days != null ? var.week_days : null
